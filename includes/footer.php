851438157f3647cd0d2e7e7cf03600322936be30
Sushil Gupta <desushil@gmail.com>
2014-01-08 13:39:53 +0545

    updated footer to include github link

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 ﻿		<!-- Footer -->
 		<footer>
-			<p style="text-align:right;"><?php echo FOOTERMESSAGE; ?></p>
+			<p style="text-align:right;"><?php echo FOOTERMESSAGE; ?> - <a href="https://github.com/guptasushil/GaunKhaneKatha">Source</a></p>
 		</footer>
 
     </div> 
